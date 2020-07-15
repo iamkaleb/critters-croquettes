69bf16db5a798805bfc416aca97e53552da748fc
iamkaleb <kalebdmoran@gmail.com>
2020-07-15 11:54:20 -0500

    initial commit

---
+++ animals.py
@@ -0,0 +1,141 @@
+from datetime import date
+
+# The petting area
+class Llama:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Donkey:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Goat:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Emu:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Camel:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+# The glass tank
+class Snake:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.slithering = True
+        self.date_added = date.today()
+
+class Lizard:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Frog:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Turtle:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Tarantula:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+# The pond
+
+class Mallard:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Flamingo:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Goldfish:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.swimming = True
+        self.date_added = date.today()
+
+class Hippopotamus:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+class Alligator:
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.walking = True
+        self.date_added = date.today()
+
+Larry = Llama('Larry', 'Llama')
+Derek = Donkey('Derek', 'Donkey')
+Gary = Goat('Gary', 'Goat')
+Eli = Emu('Eli', 'Emu')
+Carey = Camel('Carey', 'Camel')
+Sybill = Snake('Sybill', 'Snake')
+Lizzy = Lizard('Lizzy', 'Lizard')
+Felix = Frog('Felix', 'Frog')
+Tommy = Turtle('Tommy', 'Turtle')
+Terry = Tarantula('Terry', 'Tarantula')
+Mallory = Mallard('Mallory', 'Mallard')
+Ferdinand = Flamingo('Ferdinand', 'Flamingo')
+Gloria = Goldfish('Gloria', 'Goldfish')
+Harriet = Hippopotamus('Harriet', 'Hippopotamus')
+Ally = Alligator('Ally', 'Alligator')
